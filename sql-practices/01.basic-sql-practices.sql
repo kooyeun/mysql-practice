9a6123a6d5c112d519b315adfcd5190730a1ec88
kooyeun <go.orRun.straight@gmail.com>
2021-03-30 11:57:36 +0900

    Initial commit

---
+++ sql-practices/01.basic-sql-practices.sql
@@ -0,0 +1,26 @@
+
+-- pets table 생성
+create table pets(
+name varchar(20),
+owner varchar(20),
+species varchar(20),
+gender char(1),
+birth date,
+death date
+);
+
+-- table schema 확인
+desc pets;
+
+-- insert
+insert into pets values('성탄이','kickscar','dog','m','2010-12-25',null);
+insert into pets(owner,name,species,gender,birth) 
+values('kickscar','choco','cat','m','2010-12-25');
+insert into pets values('마음이','kickscar','dog','m','2010-12-25','2021-03-30');
+
+-- select
+select * from pets;
+select name,birth from pets;
+select name,birth from pets order by birth asc;
+select count(*) from pets; -- null이 있는 컬럼은 세지않는다는 것을 주의
+select count(*) from pets where death is not null;
