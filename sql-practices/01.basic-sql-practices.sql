9f2e8814c98ee027f3ebfd64de6816ae525960ef
kooyeun <go.orRun.straight@gmail.com>
2021-03-30 17:48:22 +0900

    sql문제 풀었음

--- sql-practices/01.basic-sql-practices.sql
+++ sql-practices/01.basic-sql-practices.sql
@@ -12,15 +12,20 @@ death date
 -- table schema 확인
 desc pets;
 
--- insert
+-- insert(C)
 insert into pets values('성탄이','kickscar','dog','m','2010-12-25',null);
 insert into pets(owner,name,species,gender,birth) 
 values('kickscar','choco','cat','m','2010-12-25');
 insert into pets values('마음이','kickscar','dog','m','2010-12-25','2021-03-30');
 
--- select
+-- select(R)
 select * from pets;
 select name,birth from pets;
 select name,birth from pets order by birth asc;
 select count(*) from pets; -- null이 있는 컬럼은 세지않는다는 것을 주의
 select count(*) from pets where death is not null;
+
+-- update(U)
+update pets
+set species='monkey'
+where name='choco';
